c793056d24db458f4aaf3127b4dc338030ce53e4
アーバンプロジェクトサイト課 <dsshare@sunpla.com>
2025-10-08 09:14:03 +0000

    --

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/09_copy.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/09_copy.php
@@ -1,8 +1,9 @@
-    <div class="btnFooter" data-lenis-prevent>
-        <a href="#scrolltop">
-            <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/btnFooterTop.svg" alt="ページトップに戻る" width="30" height="30">
-        </a>
-    </div>
-    <footer class="bg_171717 t_center cl_fff fw_500 maru footer">
-        ©歌って踊れるお店　角煮 . All Rights Reserved.
-    </footer>
+<div class="btnFooter" data-lenis-prevent>
+    <a href="#scrolltop">
+        <img loading="lazy" src="<?php echo get_bloginfo('template_url'); ?>/img/btnFooterTop.svg" alt="ページトップに戻る" width="30" height="30">
+    </a>
+</div>
+<footer class="bg_171717 t_center cl_fff fw_500 maru footer">
+    ©歌って踊れるお店　角煮 . All Rights Reserved.<br>
+    私達は兵庫の人と人を結ぶ旅する自由なコンビ-<a class="cl_fff" href="https://gokomusubi.com/" target="_blank">ごこむすび</a>を応援します。
+</footer>
