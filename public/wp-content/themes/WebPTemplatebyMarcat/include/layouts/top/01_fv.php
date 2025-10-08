6065a276b1b615103deb36f9e230bc273e2f784d
アーバンプロジェクトサイト課 <dsshare@sunpla.com>
2025-10-08 08:59:24 +0000

    --

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/01_fv.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/01_fv.php
@@ -1,8 +1,10 @@
 <?php if (SCF::get('fvMovie')): ?>
-<div class="fvMovie">
-    <div class="fvMovieLxn">
-        <?php echo SCF::get('fvMovie'); ?>
-    </div>
+    <div class="fvMovie">
+        <div class="fvMovieLxn">
+            <video autoplay muted loop playsinline preload="auto" style="width:100%;height:auto;object-fit:cover;">
+                <source src="<?php echo esc_url(wp_upload_dir()['baseurl'] . '/indexFv.mp4'); ?>" type="video/mp4">
+            </video>
+        </div>
 
-</div>
+    </div>
 <?php endif; ?>
--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/08_access.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/08_access.php
@@ -73,7 +73,7 @@
             </div>
         </div>
         <div class="accessMap">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.026446088867!2d135.05274647574132!3d34.62877197294538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600083bd4ddc1f13%3A0xc432e2266cbe1fcc!2z44CSNjU1LTA4OTIg5YW15bqr55yM56We5oi45biC5Z6C5rC05Yy65bmz56Ov77yU5LiB55uu77yU4oiS77yR77yUIDHpmo4!5e0!3m2!1sja!2sjp!4v1759401053695!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d976.0460814927706!2d135.0540537197128!3d34.629071229782376!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600083006514a2ad%3A0xec0ab4b157d317c7!2z5q2M44Gj44Gm6LiK44KM44KL44GK5bqXIOinkueFrg!5e0!3m2!1sja!2sjp!4v1759744203885!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="btnAccessLxn">
             <a class="d_flex j_center ali_center bg_241A08 cl_fff fw_700 kaisei btnAccess" href="https://www.google.com/maps/dir/?api=1&destination=34.628725,135.0552711&z=10" target="_blank">
