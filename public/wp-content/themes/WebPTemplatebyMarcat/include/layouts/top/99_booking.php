ce9817af6d5bebf9bb052e1e952e57f2ce7718d6
Naoki Soga <n.soga@sunplacom>
2025-11-04 12:59:35 +0900

    ---

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/99_booking.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/99_booking.php
@@ -1,4 +1,3 @@
-<?php if (have_posts()) while (have_posts()) : the_post();  ?>
     <div id="booking" class="booking">
         <section class="secBooking">
             <h2 class="t_center cl_000 fw_700 h2Booking">ご予約</h2>
@@ -7,4 +6,3 @@
             </div>
         </section>
     </div>
-<?php endwhile;  ?>
