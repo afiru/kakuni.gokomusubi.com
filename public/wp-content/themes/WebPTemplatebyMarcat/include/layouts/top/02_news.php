ee25fec1ecfd0ac566f30d932825ab4cf96ab6f4
Naoki Soga <n.soga@sunplacom>
2025-11-06 13:35:26 +0900

    --

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/02_news.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/top/02_news.php
@@ -1,25 +1,3 @@
-<section class="bg_000 nowNewsCnt jsnowNewsCnt">
-    <h2 class="t_center cl_fff fw_700 h2NowNewsCnt">今日のおすすめメニュー</h2>
-    <p class="t_center cl_fff fw_400 txtNowNewsCntTop">ビール</p>
-
-    <h2 class="t_center cl_fff fw_700 h2NowNewsCnt h2NowNewsCnt02">現在のお店の状況</h2>
-    <time class="d_block t_center cl_fff fw_400 txtNowNewsCnt">2025-11-04 10:12:15</time>
-    <ul class="d_flex j_center ali_center ulNowNewsCnt">
-        <li class="d_flex j_start ali_center liNowNewsCnt">
-            <h3 class="cl_fff fw_600 h3liNowNewsCnt">カウンター：</h3>
-            <p class="cl_fff fw_400 txtliNowNewsCnt">残り<span class="fw_700">2</span>席</p>
-        </li>
-        <li class="d_flex j_start ali_center liNowNewsCnt">
-            <h3 class="cl_fff fw_600 h3liNowNewsCnt">2人テーブル：</h3>
-            <p class="cl_fff fw_400 txtliNowNewsCnt">残り<span class="fw_700">2</span>卓</p>
-        </li>
-        <li class="d_flex j_start ali_center liNowNewsCnt">
-            <h3 class="cl_fff fw_600 h3liNowNewsCnt">4人テーブル：</h3>
-            <p class="cl_fff fw_400 txtliNowNewsCnt">残り<span class="fw_700">3</span>卓</p>
-        </li>
-    </ul>
-</section>
-
 <?php
 $args = [
     'post_type' => 'post',
